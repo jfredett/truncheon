58f3bf215610a7ddee1786aaa579c8424857471d
Joe Fredette <jfredett@gmail.com>
2025-08-01 12:29:22 -0400

    ui scaffolding

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,32 @@
-#![feature(adt_const_params)]
+use std::fmt;
+use tracing_subscriber::{fmt::Subscriber, EnvFilter};
 
+use clap::{command, Parser};
+#[cfg(test)]
+pub use tracing_test;
 
-fn main() {
+use truncheon::*;
+
+pub mod ui;
+
+#[derive(Parser)]
+#[command(version, about, long_about=None)]
+struct Options {
+    headless: Option<bool>
 }
+
+// NOTE: No need to mutation test the main wrapper.
+#[tokio::main]
+async fn main() {
+    tracing::info!("Welcome to Truncheon.");
+    let options = Options::parse();
+
+
+    // Log to a file
+    let (non_blocking, _guard) = tracing_appender::non_blocking(std::fs::File::create("truncheon.log").unwrap());
+    let subscriber = Subscriber::builder()
+        .with_env_filter(EnvFilter::from_default_env())
+        .finish();
+    let _ = ui::run().await;
+}
+

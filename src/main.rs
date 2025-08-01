4131094b0c5a1208952bdbe7d08bd33fa8cd068e
Joe Fredette <jfredett@gmail.com>
2025-08-01 12:00:35 -0400

    by the yard is hard

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 #![feature(adt_const_params)]
 
-use truncheon::hex::{self, Direction, Origin};
 
 fn main() {
-    let field : hex::Hexfield<10, 10, {Origin::TopLeft}, {Direction::LR_TB}, ()> = hex::Hexfield::new();
-    println!("Debug: {:?}", field);
 }
